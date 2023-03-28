9dd044299b357e14024a7279bcee700dc1af09ed
Anna Laffoon <ree11194@gmail.com>
2023-03-28 10:00:47 -0400

    Band class with props

---
+++ Models/Band.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShowtimeManager.Models
+{
+    public class Band
+    {
+        public int Id { get; set; }
+        public string? BandName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ShowTime { get; set; }
+        public string? Genre { get; set; }
+        public double TicketPrice { get; set; }
+    }
+}
