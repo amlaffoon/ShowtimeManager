73dec4a05bf6b5852025f75e42020a1dd738950c
Anna Laffoon <ree11194@gmail.com>
2023-03-29 11:23:03 -0400

    display attribute for column headers

--- Models/Band.cs
+++ Models/Band.cs
@@ -1,14 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ShowtimeManager.Models
 {
     public class Band
     {
         public int Id { get; set; }
+        [Display(Name = "Artist")]
         public string? BandName { get; set; }
         [DataType(DataType.Date)]
+        [Display(Name = "Showtime")]
         public DateTime ShowTime { get; set; }
         public string? Genre { get; set; }
+        [Display(Name = "Ticket Price")]
         public double TicketPrice { get; set; }
     }
 }
