603361d52aca0af14962def935d5a63207bca7c4
Anna Laffoon <ree11194@gmail.com>
2023-03-30 10:40:48 -0400

    add validation

--- Models/Band.cs
+++ Models/Band.cs
@@ -7,12 +7,19 @@ namespace ShowtimeManager.Models
     {
         public int Id { get; set; }
         [Display(Name = "Artist")]
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string? BandName { get; set; }
         [DataType(DataType.Date)]
         [Display(Name = "Showtime")]
         public DateTime ShowTime { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string? Genre { get; set; }
         [Display(Name = "Ticket Price")]
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public double TicketPrice { get; set; }
     }
 }
