34066a2debdb6e2d8ce4a77f4b4b22574006075b
Anna Laffoon <ree11194@gmail.com>
2023-03-30 10:41:10 -0400

    update database

--- Migrations/20230330143831_New_DataAnnotations.Designer.cs
+++ Migrations/20230330143831_New_DataAnnotations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShowtimeManager.Data;
 
@@ -11,9 +12,11 @@ using ShowtimeManager.Data;
 namespace ShowtimeManager.Migrations
 {
     [DbContext(typeof(ShowtimeManagerContext))]
-    partial class ShowtimeManagerContextModelSnapshot : ModelSnapshot
+    [Migration("20230330143831_New_DataAnnotations")]
+    partial class New_DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
